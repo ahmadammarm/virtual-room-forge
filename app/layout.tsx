2c3980b6c68d5cde114cda720b5019e1e71c23c8
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-12-20 08:32:27 +0700

    updating layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,49 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import couch from '@/assets/couch.svg'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+import couch from "@/assets/couch.svg";
+import Image from "next/image";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
+import UserInfo from "@/components/UserInfo";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Virtual Room Forge',
-  description: 'AI Room Generate',
-}
+  title: "Virtual Room Forge",
+  description: "Virtual Room Forge",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  
   return (
-    <html lang="en">
-      <body className={inter.className + "bg-[#17181c] flex flex-col min-h-screen"}>
-        <header className='h-full'>
-          <div className='container border-b p-5 mx-auto flex items-center justify-between'>
-            <Link href={"/"} className='flex items-center gap-3'>
-              <Image src={couch} alt="VRF" width={100} height={100} />
-            </Link>
-            {/* User Info */}
-          </div>
-        </header>
-        {children}
-      </body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={
+            inter.className + " bg-[#17181c] flex flex-col min-h-screen"
+          }
+        >
+          <header className="w-full">
+            <div className="container border-b p-5 mx-auto flex items-center justify-between">
+              <Link href={"/"} className="flex items-center gap-3">
+                <Image width={50} height={50} src={couch} alt="couch logo" />
+                <h3 className="text-white md:text-3xl text-md font-bold">
+                  Virtual RF
+                </h3>
+              </Link>
+              <UserInfo/> 
+            </div>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,10 +1,21 @@
 "use client"
 
 import React from 'react'
+import { useUser, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 function UserInfo() {
-  return (
-    <div>UserInfo</div>
+    
+    const {user} = useUser()
+
+  return !user ?  (
+    <Link href={"/room"} className='bg-teal-500 hover:opacity-90 rounded-lg text-white font-semibold px-5 py-3
+    transition duration-300 ease-in-out
+    '>
+        Sign in
+    </Link>
+  ): (
+    <UserButton />
   )
 }
 
